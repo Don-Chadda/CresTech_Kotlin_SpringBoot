5b666a2aefa4fc37765a802a5b6c8616d7752e0e
Don-Chadda <peterturay81@gmail.com>
2022-12-14 12:23:25 +0000

    Created kotlin class REST Controller

--- crestech/src/main/kotlin/com/crestech/tutorials/springboot/crestech/CresTechApplication.kt
+++ crestech/src/main/kotlin/com/crestech/tutorials/springboot/crestech/CresTechApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CrestechApplication
+class CresTechApplication
 
 fun main(args: Array<String>) {
-	runApplication<CrestechApplication>(*args)
+	runApplication<CresTechApplication>(*args)
 }
--- crestech/src/test/kotlin/com/crestech/tutorials/springboot/crestech/CresTechApplicationTests.kt
+++ crestech/src/test/kotlin/com/crestech/tutorials/springboot/crestech/CresTechApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class CrestechApplicationTests {
+class CresTechApplicationTests {
 
 	@Test
 	fun contextLoads() {
